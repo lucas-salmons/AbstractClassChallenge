0378192d9e52f5f3caacf6094788ce16f1815f39
Lucas Salmons <lucsalmons@gmail.com>
2021-01-01 14:55:57 -0800

    Implemented removeItem method for BST class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         SearchTree tree = new SearchTree(null);
         tree.traverse(tree.getRoot());
 
-        String stringData = "5 7 3 9 8 2 0 4 6";
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
 
         String[] data = stringData.split(" ");
         for (String s: data){
@@ -15,5 +15,27 @@ public class Main {
         }
 
         tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("5"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("9"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("8"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("6"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+
     }
 }
